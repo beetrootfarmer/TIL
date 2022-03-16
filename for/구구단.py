998d9a82a90c76de0895c5718e33e70452908b3d
beetrootfarmer <pos04118@gmail.com>
2022-03-17 00:28:18 +0900

    구구단

---
+++ for/구구단.py
@@ -0,0 +1,21 @@
+# 1=< N =< 9 ,  N을 받아서 구구단을 출력하는 함수를 만들기 
+
+N = int(input())
+for i in range(1,10) : #range(1,10) = 1~9 !!!!!!!
+    print(N, "*", i, "=" +(N*i))
+
+# java로 하면
+# import java.util.Scanner;
+# public class Main {
+# 	public static void main(String[] args) {
+ 
+# 		Scanner in = new Scanner(System.in);	
+# 		int a = in.nextInt();
+		
+# 		in.close();		
+		
+# 		for(int i = 1; i<10;i++) {
+# 			System.out.println(a+" * "+i+" = "+(a*i));
+# 		}
+# 	}
+# }
