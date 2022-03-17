a4c3f3a7cceb19d1ac65cfcce5b10ce8736e0cef
beetrootfarmer <pos04118@gmail.com>
2022-03-17 20:15:11 +0900

    while

--- for/10871_x보다 작은 수.py
+++ for/10871_x보다 작은 수.py
@@ -1,7 +1,7 @@
 # N은 입력할 정수 개수, X는 기준 정수. X보다 작은 수를 출력
 N, X = map(int, input().split())
 a = list(map(int, input().split()))
-for i in range(N-1) :
-    if ((X-1) < a[i]) :
-        del a[i]
-print(*a, sep=' ')#sep : 지정문자를 사용해서 요소를 구분
+for i in range(N) :
+    if (a[i] < X) :
+        print(a[i], end=' ')#end : 기본값은 줄바꿈인데 , 지정 문자로 마치기
+                            #sep : 리스트 구분 기본값은 쉽표, 지정문자를 사용해서 요소를 구분
