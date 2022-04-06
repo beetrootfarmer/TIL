461fd7aa7446a16d3171f31e4434a9722440dbcf
beetrootfarmer <pos04118@gmail.com>
2022-04-06 20:22:00 +0900

    k번째2

--- programmers/k번째 수.py
+++ programmers/k번째 수.py
@@ -51,3 +51,7 @@ array = [1, 5, 2, 6, 3, 7, 4]
 commands = [[2,5,3],[4,4,1],[1,7,3]]
 print(solution(array,commands))
 
+
+#============lamda 식을 써서 한줄로 끝내버림??==========
+def solution(array, commands):
+    return list(map(lambda x:sorted(array[x[0]-1:x[1]])[x[2]-1], commands))
