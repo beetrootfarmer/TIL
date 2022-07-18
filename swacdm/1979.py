a1911210f7c39d89ea8a855ab08931b445917bfc
beetrootfarmer <pos04118@gmail.com>
2022-07-18 13:08:21 +0900

    pypy hw1

--- swacdm/1979.py
+++ swacdm/1979.py
@@ -2,10 +2,18 @@ test_case = int(input())
 for case in range(test_case):
     N , K = map(int,input().split()) #N = board size, K = line size
     board = []
-    ver = []
+    hor_cnt , ver_cnt, answer = 0
     for i in range(N):
         board[i].append(list(map(int,input().split())))
-    for v in board:
-        for h in board[v]:
-            if h :
-                print()
+    for v in range(0,N): #(0,len(board)):
+        ver = [0]*N
+        for h in range(0,N): #board[v]:
+            if board[v][h] == 1 :
+                hor_cnt += 1
+                ver[h] += 1
+            elif hor_cnt == K:
+                answer += 1
+                hor_cnt = 0
+            elif ver[h] == K:
+                answer += 1
+                ver[h] = 0
