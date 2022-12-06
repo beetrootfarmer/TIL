1dcb08b842ddf0118437bb46b89583b54aa71b4f
beetrootfarmer <pos04118@gmail.com>
2022-12-06 23:20:21 +0900

    1206

--- Algorithm/Algorithm_매일알고/12873 기념품/sol.py
+++ Algorithm/Algorithm_매일알고/12873 기념품/sol.py
@@ -1,1 +1,29 @@
 import sys
+input = sys.stdin.readline
+
+N = int(input())    # 참가자 N명
+                    # 참가자가 1명 남을때까지 t**3 배수로 참가자를 제거한다
+                    # 시계방향으로 회전하면서 제거
+                    # N-1번 반복하면서 제거
+t = 1
+next = 0
+students = list(range(1,N+1))
+print(students)
+for _ in range(1,N):
+
+    count = t**3
+    if (count + next) > len(students):
+        count = (count + next) % (len(students)) # 시작점 기준으로 몇번째 사람이 제거되는지 배열의 길이 미만으로 만듦
+    a = students.pop(count-1)
+    print('a', a)
+    print('next', next, 'count', count)
+    print('students', students)
+    next += count-1
+    # if next >= len(students):
+    #     next = next % len(students)
+    # if next + count > len(students):
+
+
+    # next = count-1      # 한명이 빠지면 그 다음사람부터 시작
+
+    t+=1
