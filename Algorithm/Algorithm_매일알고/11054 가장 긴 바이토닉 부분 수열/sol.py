c100ca4335037b17878a076be9ba90c5c91b2022
beetrootfarmer <pos04118@gmail.com>
2022-12-20 23:48:26 +0900

    1220

--- Algorithm/Algorithm_매일알고/11054 가장 긴 바이토닉 부분 수열/sol.py
+++ Algorithm/Algorithm_매일알고/11054 가장 긴 바이토닉 부분 수열/sol.py
@@ -5,8 +5,7 @@ data = list(map(int,sys.stdin.readline().split()))
 dp = [1] * N                                
 
 for i in range(1, N):                       
-    for j in range(i):
-                       
+    for j in range(i): 
         if data[i] > data[j]:    
             print('i =', i, ', j=', j)                  
             dp[i] = max(dp[j] + 1, dp[i])   
