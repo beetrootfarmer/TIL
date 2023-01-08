3c1e5dedc4f6e98dbc7a901fbd2e648a76f5b054
beetrootfarmer <pos04118@gmail.com>
2023-01-08 23:00:53 +0900

    18

---
+++ React/movie-app/src/App.js
@@ -0,0 +1,20 @@
+import {useEffect, useMemo, useState} from "react";
+function App() {
+  const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+  const getMovies = async () => {
+    const json = await(
+      await fetch(
+        'https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year'
+      )
+    ).json();
+    setMovies(json.data.movies));
+    setLoading(false);
+    };
+    useEffect(() => {
+      getMovies();
+    }, []);
+  return <div>{loading ? <h1>Loading...</h1> : <div>{movies.map()}</div>}</div>;
+}
+
+export default App;
